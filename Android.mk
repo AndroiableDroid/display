95a48d442148945faa5f5d675f24045b4b04030f
Naseer Ahmed <naseer@codeaurora.org>
2017-05-15 12:20:07 -0400

    display: Partially convert makefiles to blueprints
    Change-Id: I5429ecee3985e305290e312921725460ef195b83
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,10 +1,9 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-display))
 display-hals := include libgralloc libgenlock libcopybit
-display-hals += libhwcomposer liboverlay libqdutils libhdmi libqservice
+display-hals += libhwcomposer liboverlay libhdmi
 display-hals += libmemtrack
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
-endif
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
